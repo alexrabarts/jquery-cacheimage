54a7105361624a9834abc68002a2827d25773aa2
Alex Rabarts <alexrabarts@gmail.com>
2013-02-14 14:32:35 +0000

    Add jQuery plugins manifest and version bump to 1.0.0

--- jquery.cacheimage.js
+++ jquery.cacheimage.js
@@ -1,22 +1,8 @@
 /*
  * cacheImage: a jQuery plugin
  *
- * cacheImage is a simple jQuery plugin for pre-caching images.  The
- * plugin can be used to eliminate flashes of unstyled content (FOUC) and
- * improve perceived page load time.  Callbacks for load, error and abort
- * events are provided.
- *
- * For usage and examples, visit:
- * http://github.com/alexrabarts/jquery-cacheimage
- *
  * Licensed under the MIT:
  * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright (c) 2008 Stateless Systems (http://statelesssystems.com)
- *
- * @author   Alex Rabarts (alexrabarts -at- gmail -dawt- com)
- * @requires jQuery v1.2 or later
- * @version  0.2.1
  */
 
 (function ($) {
